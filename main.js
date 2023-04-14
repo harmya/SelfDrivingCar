d534771b81865d7cfa3e8094b9a283f934384efe
Harmya Bhatt <60983274+harmya@users.noreply.github.com>
2023-04-14 04:32:50 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.height = window.innerHeight;
+const deviceWidth = window.innerWidth;
+canvas.width = deviceWidth / 5;
